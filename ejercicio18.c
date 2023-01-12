e7dcb1e2de592f46c73227458def78b1f5b22150
Hugolino Valentin <104662717+Pulgarcit0@users.noreply.github.com>
2023-01-12 16:32:56 -0600

    Create ejercicio18.c
    18

---
+++ ejercicio18.c
@@ -0,0 +1,12 @@
+int main(){
+    int nume=10;
+    for (int i=0;i<=10;i++){
+        for (int j=1;j<=10;j++){
+            printf(" %d",i);
+            printf("*%d",j);
+            printf("->%d",(i*j));
+            printf("\n");
+        }
+        printf(" \n");
+    }
+}
